13bbe48bb6604f4840f1c2f44160e81220ab6135
nichady <nichthai2004@gmail.com>
2024-10-03 22:30:18 -0500

    format code

--- shared/src/iosMain/kotlin/com/github/csc3380fall2024/team16/Platform.ios.kt
+++ shared/src/iosMain/kotlin/com/github/csc3380fall2024/team16/Platform.ios.kt
@@ -2,7 +2,7 @@ package com.github.csc3380fall2024.team16
 
 import platform.UIKit.UIDevice
 
-class IOSPlatform: Platform {
+class IOSPlatform : Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
 }
 
--- shared/src/jvmMain/kotlin/com/github/csc3380fall2024/team16/Platform.jvm.kt
+++ shared/src/jvmMain/kotlin/com/github/csc3380fall2024/team16/Platform.jvm.kt
@@ -1,6 +1,6 @@
 package com.github.csc3380fall2024.team16
 
-class JVMPlatform: Platform {
+class JVMPlatform : Platform {
     override val name: String = "Java ${System.getProperty("java.version")}"
 }
 
