12a5f3979cf073584258682905cd3cc73940d8f3
nichady <nichthai2004@gmail.com>
2024-11-29 09:46:35 -0600

    redo run configuration

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,3 +1,5 @@
+val envVars = listOf("SERVER_PORT")
+
 plugins {
     alias(libs.plugins.kotlinJvm)
     alias(libs.plugins.ktor)
@@ -10,10 +12,23 @@ application {
     mainClass = "com.github.csc3380fall2024.team16.ApplicationKt"
 }
 
-tasks.named<JavaExec>("run") {
-    listOf("SERVER_PORT").forEach {
-        environment(it, env.fetch(it))
+tasks.register("generateDevelopmentConfig") {
+    val resourcesDir = layout.buildDirectory.dir("resources/main").get().asFile
+    inputs.file(rootDir.resolve(".env"))
+    outputs.dir(resourcesDir)
+    
+    doLast {
+        val file = resourcesDir.resolve("config.properties")
+        file.parentFile.mkdirs()
+        
+        var text = ""
+        envVars.forEach { text += "$it = ${env.fetch(it)}\n" }
+        file.writeText(text)
     }
+}
+
+tasks.compileJava {
+    dependsOn("generateDevelopmentConfig")
 }
 
 dependencies {
--- server/src/main/kotlin/com/github/csc3380fall2024/team16/Application.kt
+++ server/src/main/kotlin/com/github/csc3380fall2024/team16/Application.kt
@@ -1,13 +1,15 @@
 package com.github.csc3380fall2024.team16
 
+import com.github.csc3380fall2024.team16.plugins.config
 import com.github.csc3380fall2024.team16.plugins.configureDatabase
 import com.github.csc3380fall2024.team16.plugins.configureRpc
 import io.ktor.server.application.Application
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
+
 fun main() {
-    val port = env("SERVER_PORT").toInt()
+    val port = config("SERVER_PORT").toInt()
     
     embeddedServer(Netty, port = port, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
@@ -17,6 +19,3 @@ fun Application.module() {
     configureDatabase()
     configureRpc()
 }
-
-fun env(name: String) =
-    System.getenv(name) ?: throw Exception("environment variable $name not set")
