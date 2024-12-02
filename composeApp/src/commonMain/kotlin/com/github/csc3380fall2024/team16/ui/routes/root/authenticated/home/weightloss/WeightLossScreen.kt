aa39472b9f7062e6268e016bbcf9d37597103495
chanhnny <leleschoolwork2022@gmail.com>
2024-12-02 17:07:39 -0600

    search filter weightloss page

--- composeApp/src/commonMain/kotlin/com/github/csc3380fall2024/team16/ui/routes/root/authenticated/home/weightloss/WeightLossScreen.kt
+++ composeApp/src/commonMain/kotlin/com/github/csc3380fall2024/team16/ui/routes/root/authenticated/home/weightloss/WeightLossScreen.kt
@@ -3,12 +3,18 @@ package com.github.csc3380fall2024.team16.ui.routes.root.authenticated.home.weig
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -17,6 +23,13 @@ import com.github.csc3380fall2024.team16.ui.components.ExerciseItem
 
 @Composable
 fun WeightLossScreen(onBack: () -> Unit) {
+    
+    var searchQuery by remember { mutableStateOf("") }
+    val exercises = ExerciseRepository.getWeightLossExercises()
+    val filteredExercises = exercises.filter { exercise ->
+        exercise.name.contains(searchQuery, ignoreCase = true)
+    }
+    
     Box(Modifier.fillMaxSize()) {
         // Back Button in the top-left corner
         Button(
@@ -34,14 +47,21 @@ fun WeightLossScreen(onBack: () -> Unit) {
         ) {
             Text("Weight Loss Exercises", fontSize = 24.sp, modifier = Modifier.padding(8.dp))
             
-            // Fetch exercises list from repository
-            val exercises = ExerciseRepository.getWeightLossExercises()
+            TextField(
+                value = searchQuery,
+                singleLine = true,
+                onValueChange = { searchQuery = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                placeholder = { Text("Search exercises...") }
+            )
             
             LazyColumn(
                 modifier = Modifier.fillMaxSize()
             ) {
                 // Populate LazyColumn with Exercise items
-                items(exercises) { exercise ->
+                items(filteredExercises) { exercise ->
                     ExerciseItem(exercise)
                 }
             }
