b24e41ac78929f6b1f17384451a9b871229e5231
petern12g <peternguyensmhs@gmail.com>
2024-12-10 15:44:10 -0600

    add password masking on login page and reveal button

--- composeApp/src/commonMain/kotlin/com/github/csc3380fall2024/team16/ui/routes/root/unauthenticated/login/LoginScreen.kt
+++ composeApp/src/commonMain/kotlin/com/github/csc3380fall2024/team16/ui/routes/root/unauthenticated/login/LoginScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -20,6 +21,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -31,6 +34,7 @@ fun LoginScreen(
 ) {
     var usernameOrEmail by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var passwordMask by remember { mutableStateOf(false) }
     
     Column(
         Modifier.fillMaxSize().padding(horizontal = 20.dp),
@@ -59,7 +63,14 @@ fun LoginScreen(
                 Modifier.fillMaxWidth(),
                 placeholder = { Text("Password") },
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                singleLine = true
+                singleLine = true,
+                visualTransformation = if (passwordMask) VisualTransformation.None else PasswordVisualTransformation()
+            )
+            
+            Text(
+                text = "Reveal Password",
+                modifier = Modifier.padding(start = 8.dp).wrapContentWidth().clickable { passwordMask = !passwordMask },
+                color = MaterialTheme.colorScheme.primary
             )
         }
         
